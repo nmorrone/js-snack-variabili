07c8e165e91485d10598df2bb2a7e980d4f38986
nmorrone <n.morrone96@gmail.com>
2024-07-11 16:48:14 +0200

    esercizio 1

---
+++ JS/esercizio-1.js
@@ -0,0 +1,8 @@
+
+/*dichiarazione e assegnazione valore variabile
+dichiaro con const perché non ho bisogno di fare calcoli ed è un valore constante/fisso */
+const number = '100'
+
+//stampo in console per verificare
+
+console.log(number)
