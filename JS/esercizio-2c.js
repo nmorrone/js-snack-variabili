a148e0f7b1a8822db70768b5276eee91809a8349
nmorrone <n.morrone96@gmail.com>
2024-07-12 09:47:44 +0200

    esercizio-2c

---
+++ JS/esercizio-2c.js
@@ -0,0 +1,16 @@
+//dichiaro constant PI e assegno valore
+const PI = '3.14'
+//stampo in console
+console.log(PI)
+
+//dichiaro variabile chiamata radius
+const radius = '8'
+
+//stampo in console
+console.log(radius)
+
+//concatenazione variabile circle
+circle = radius * 2 * PI
+
+//stampo in console
+console.log(circle)
