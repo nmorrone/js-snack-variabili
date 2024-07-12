58504011bf19fc597a1db0e6d6beef272e806537
nmorrone <n.morrone96@gmail.com>
2024-07-12 09:41:12 +0200

    esercizio 2

--- JS/esercizio-2.js
+++ JS/esercizio-2.js
@@ -3,17 +3,3 @@ const PI = '3.14'
 //stampo in console
 console.log(PI)
 
-//assegno nuovo valore alla constante
-const PI = '3.1416'
-//stampo di nuovo in console
-console.log(PI)
-
-/* siccome PI è una constante e abbiamo dato il valore iniziale 3.14 
-provando a riassegnare un valore a PI la console stamperà un errore
-dove SyntaxError 'PI' already been declared */
-
-let PI = '3.1416'
-
-console.log(PI)
-
-//neanche con let poiché const fissa il valore iniziale, di nuovo erroe
